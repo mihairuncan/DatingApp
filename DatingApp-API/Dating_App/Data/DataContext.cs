4d8c2aeeea2baea653b644fb0ff66d46162d53e4
Mihai <runcan.mihai@gmail.com>
2020-05-30 00:20:16 +0300

    project initialization

---
+++ DatingApp-API/Dating_App/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Dating_App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating_App.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            :base(options)
+        {
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
