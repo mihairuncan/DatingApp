2201ece8d2d0981293620b10904f1f4133c4f76f
Mihai <runcan.mihai@gmail.com>
2020-05-31 19:42:03 +0300

    implemented login and register in UI

--- DatingApp-API/Dating_App/Controllers/ValuesController.cs
+++ DatingApp-API/Dating_App/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace Dating_App.Controllers
             _context = context;
         }
 
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
